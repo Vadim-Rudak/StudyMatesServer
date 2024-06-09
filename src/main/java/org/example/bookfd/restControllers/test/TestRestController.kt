a1f582f66debb691bb3ab9326c00809a5474a0a2
Vadim Rudak <rudak.v@mail.ru>
2024-06-09 09:53:50 +0200

    edit test rest controller

--- src/main/java/org/example/bookfd/restControllers/test/TestRestController.kt
+++ src/main/java/org/example/bookfd/restControllers/test/TestRestController.kt
@@ -1,8 +1,10 @@
 package org.example.bookfd.restControllers.test
 
 import org.example.bookfd.domain.Questions
+import org.example.bookfd.domain.Results
 import org.example.bookfd.domain.TestsNames
 import org.example.bookfd.repos.QuestionsRepo
+import org.example.bookfd.repos.ResultsRepo
 import org.example.bookfd.repos.TestsNamesRepo
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
@@ -11,7 +13,8 @@ import org.springframework.web.bind.annotation.*
 @RestController
 class TestRestController @Autowired constructor(
     private val testsNamesRepo: TestsNamesRepo,
-    private val questionsRepo: QuestionsRepo
+    private val questionsRepo: QuestionsRepo,
+    private val resultsRepo: ResultsRepo
 ) {
 
     private var idTest = 0
@@ -19,11 +22,7 @@ class TestRestController @Autowired constructor(
     /*
         Get list tests in selected subject
     */
-    @RequestMapping(
-        value = ["/ListTestsNames"],
-        method = [RequestMethod.GET],
-        produces = [MediaType.APPLICATION_JSON_VALUE]
-    )
+    @RequestMapping(value = ["/ListTestsNames"], method = [RequestMethod.GET], produces = [MediaType.APPLICATION_JSON_VALUE])
     fun getListTests(@RequestParam(name="subject", required=false, defaultValue="физика") subject:String) = testsNamesRepo.findBySubject(subject)
 
     /*
@@ -44,5 +43,19 @@ class TestRestController @Autowired constructor(
     fun addTest(@RequestBody testsNames: TestsNames){
         testsNamesRepo.save(testsNames)
         idTest = testsNames.id
+    }
+
+    /*
+        Get list questions in selected test
+    */
+    @RequestMapping(value = ["/Questions"], method = [RequestMethod.GET], produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun getQuestionsInTest(@RequestParam(name="test_id", required=false, defaultValue="") testId:Int) = questionsRepo.findByTestid(testId)
+
+    /*
+        Add test result
+    */
+    @PostMapping("/add_result")
+    fun addTestResult (@RequestBody results: Results){
+        resultsRepo.save(results)
     }
 }
